323b9b4e0ca05d6c56a0c2f2d7d8d47363e612b7
Thomas Pöchtrager <t.poechtrager@gmail.com>
2025-08-16 12:37:48 +0200

    Fix _dispatch_muxnote_armed_events()

--- src/event/event_epoll.c
+++ src/event/event_epoll.c
@@ -89,7 +89,7 @@ DISPATCH_ALWAYS_INLINE
 static inline uint32_t
 _dispatch_muxnote_armed_events(dispatch_muxnote_t dmn)
 {
-	return dmn->dmn_events & ~dmn->dmn_disarmed_events;
+	return ((uint32_t)dmn->dmn_events) & ~((uint32_t)dmn->dmn_disarmed_events);
 }
 
 DISPATCH_ALWAYS_INLINE
